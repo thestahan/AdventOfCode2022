6f36b040bfac1a8ef4a0d3fa3b16453569554f09
Kacper <thestahan@gmail.com>
2022-12-03 10:37:08 +0100

    Submit day 3 solution

---
+++ 03_rucksack_reorganization/Program.cs
@@ -0,0 +1,43 @@
+﻿string path = Path.Combine(Environment.CurrentDirectory, "input.txt");
+
+var inputData = File.ReadAllLines(path);
+
+int sumOfPriorities = 0;
+
+foreach (var rucksack in inputData)
+{
+    var compartments = rucksack.Chunk(rucksack.Length / 2);
+
+    char priority = compartments.ElementAt(0).Intersect(compartments.ElementAt(1)).First();
+
+    if (char.IsUpper(priority))
+    {
+        sumOfPriorities += ((int)priority - 38);
+    }
+    else if (char.IsLower(priority))
+    {
+        sumOfPriorities += ((int)priority - 96);
+    }
+}
+
+Console.WriteLine($"Part one answer: {sumOfPriorities}");
+
+var elvesGroups = inputData.Chunk(3);
+
+sumOfPriorities = 0;
+
+foreach (var elvesGroup in elvesGroups)
+{
+    var priority = elvesGroup.Aggregate((previousGroup, nextGroup) => string.Concat(previousGroup.Intersect(nextGroup))).First();
+
+    if (char.IsUpper(priority))
+    {
+        sumOfPriorities += ((int)priority - 38);
+    }
+    else if (char.IsLower(priority))
+    {
+        sumOfPriorities += ((int)priority - 96);
+    }
+}
+
+Console.WriteLine($"Part two answer: {sumOfPriorities}");
