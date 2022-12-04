4abd3cc5bfe73c5db664d7e7327116d69c51a829
Kacper <thestahan@gmail.com>
2022-12-04 06:24:06 +0100

    Submit day 4 solution

---
+++ 04_camp_cleanup/Program.cs
@@ -0,0 +1,38 @@
+﻿string path = Path.Combine(Environment.CurrentDirectory, "input.txt");
+
+var inputData = File.ReadAllLines(path);
+
+int totalPartOne = 0;
+
+int totalPartTwo = 0;
+
+foreach (var item in inputData)
+{
+    var elvesData = item.Split(",");
+
+    var firstElfData = elvesData.ElementAt(0).Split("-");
+
+    var secondElfData = elvesData.ElementAt(1).Split("-");
+
+    var firstElfFrom = Int32.Parse(firstElfData.ElementAt(0));
+    var firstElfTo = Int32.Parse(firstElfData.ElementAt(1));
+
+    var secondElfFrom = Int32.Parse(secondElfData.ElementAt(0));
+    var secondElfTo = Int32.Parse(secondElfData.ElementAt(1));
+
+    if (firstElfFrom >= secondElfFrom && firstElfTo <= secondElfTo ||
+        secondElfFrom >= firstElfFrom && secondElfTo <= firstElfTo)
+    {
+        totalPartOne++;
+    }
+
+    if (firstElfFrom >= secondElfFrom && firstElfTo <= secondElfTo ||
+        secondElfFrom >= firstElfFrom && secondElfTo <= firstElfTo ||
+        firstElfTo >= secondElfFrom && firstElfFrom <= secondElfTo)
+    {
+        totalPartTwo++;
+    }
+}
+
+Console.WriteLine(totalPartOne);
+Console.WriteLine(totalPartTwo);
