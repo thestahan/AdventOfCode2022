f9bb193ad5d6b6ab70d832c350074b50f61304c6
Kacper <thestahan@gmail.com>
2022-12-06 06:14:56 +0100

    Submit day 6 solution

---
+++ 06_tuning_trouble/Program.cs
@@ -0,0 +1,25 @@
+﻿string path = Path.Combine(Environment.CurrentDirectory, "input.txt");
+
+var inputData = File.ReadAllLines(path).First();
+
+var charsQueue = new Queue<char>();
+
+for (int i = 0; i < 14; i++)
+{
+    charsQueue.Enqueue(inputData[i]);
+}
+
+for (int i = 14; i < inputData.Length; i++)
+{
+    var test = charsQueue.Select(x => x).ToList();
+
+    if (test.GroupBy(x => x).ToDictionary(y => y.Key, y => y.Count()).All(x => x.Value == 1))
+    {
+        Console.WriteLine(i);
+
+        break;
+    }
+
+    charsQueue.Dequeue();
+    charsQueue.Enqueue(inputData[i]);
+}
